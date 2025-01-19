e61baaaaf1e77ca6fe6626ea938c088a2bcf142b
Asha Koen <ashakoen@gmail.com>
2025-01-19 12:16:59 -0500

    update bucket limits

--- src/constants/storage.ts
+++ src/constants/storage.ts
@@ -1,5 +1,5 @@
 export const STORAGE = {
     EXPIRY_TIME_MS: 3600000, // 1 hour
     CLEANUP_INTERVAL_MS: 60000, // 1 minute
-    MAX_BUCKET_IMAGES: 30, // Maximum images in bucket
+    MAX_BUCKET_IMAGES: 100, // Maximum images in bucket
 } as const;
