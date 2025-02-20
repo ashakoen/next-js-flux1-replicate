4ae4552bce89340ed342cc38e600b082dd6d16bf
Asha Koen <ashakoen@gmail.com>
2025-02-20 17:24:39 -0500

    add speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'next-themes';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
             {children}
           </main>
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
